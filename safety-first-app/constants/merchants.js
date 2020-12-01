33d85eac7e5faa0107490c1fd4ea8dba2ad84a23
Mary Floren <maryfloren@Marys-MacBook-Pro.local>
2020-12-01 14:01:12 -0600

    added notification screens

---
+++ safety-first-app/constants/merchants.js
@@ -0,0 +1,149 @@
+export default [
+    {
+        name: 'Walgreens',
+        waitTime: {
+            current: '6',
+            waitOneHour: '8',
+            waitFiveHour: '10',
+            tomorrow: '4'
+        },
+        streetAddress: '4170 El Camino Real',
+        city: 'Palo Alto',
+        state: 'CA',
+        zip: '94306',
+        country: 'USA',
+        distance: '.8',
+        distanceUnit: 'm',
+        latitude: '37.367755',
+        longitude: '-121.991990',
+        category: '',
+        terminalType: [],
+        firstTranDateRange: '',
+        lastTranDateRange: '',
+        cta: 'View Store',
+        image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+    },
+    {
+        name: 'Panera Bread',
+        waitTime: {
+            current: '6',
+            waitOneHour: '8',
+            waitFiveHour: '10',
+            tomorrow: '4'
+        },
+        streetAddress: '660 Stanford Shopping Center',
+        city: 'Palo Alto',
+        state: 'CA',
+        zip: '94304',
+        country: 'USA',
+        distance: '1',
+        distanceUnit: 'm',
+        latitude: '37.363355',
+        longitude: '-121.921990',
+        category: 'FAST CASUAL RESTAURANT',
+        terminalType: [],
+        firstTranDateRange: '',
+        lastTranDateRange: '',
+        cta: 'View Store',
+        image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    },
+    {
+        name: 'Walgreens',
+        waitTime: {
+            current: '6',
+            waitOneHour: '8',
+            waitFiveHour: '10',
+            tomorrow: '4'
+        },
+        streetAddress: '4170 El Camino Real',
+        city: 'Palo Alto',
+        state: 'CA',
+        zip: '94306',
+        country: 'USA',
+        distance: '.8',
+        distanceUnit: 'm',
+        latitude: '37.367755',
+        longitude: '-121.991990',
+        category: '',
+        terminalType: [],
+        firstTranDateRange: '',
+        lastTranDateRange: '',
+        cta: 'View Store',
+        image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    },
+    {
+        name: 'Panera Bread',
+        waitTime: {
+            current: '6',
+            waitOneHour: '8',
+            waitFiveHour: '10',
+            tomorrow: '4'
+        },
+        streetAddress: '660 Stanford Shopping Center',
+        city: 'Palo Alto',
+        state: 'CA',
+        zip: '94304',
+        country: 'USA',
+        distance: '1',
+        distanceUnit: 'm',
+        latitude: '37.363355',
+        longitude: '-121.921990',
+        category: 'FAST CASUAL RESTAURANT',
+        terminalType: [],
+        firstTranDateRange: '',
+        lastTranDateRange: '',
+        cta: 'View Store',
+        image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    },
+    {
+        name: 'Panera Bread',
+        waitTime: {
+            current: '6',
+            waitOneHour: '8',
+            waitFiveHour: '10',
+            tomorrow: '4'
+        },
+        streetAddress: '660 Stanford Shopping Center',
+        city: 'Palo Alto',
+        state: 'CA',
+        zip: '94304',
+        country: 'USA',
+        distance: '1',
+        distanceUnit: 'm',
+        latitude: '37.363355',
+        longitude: '-121.921990',
+        category: 'FAST CASUAL RESTAURANT',
+        terminalType: [],
+        firstTranDateRange: '',
+        lastTranDateRange: '',
+        cta: 'View Store',
+        image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    }
+] 
+
+
+
+
+
+
+
+
+/*
+{
+        name: '',
+        waitTime: '',
+        streetAddress: '',
+        city: '',
+        state: '',
+        zip: '',
+        country: '',
+        distance: '',
+        distanceUnit: '',
+        latitude: '',
+        longitude: '',
+        category: '',
+        terminalType: [],
+        firstTranDateRange: '',
+        lastTranDateRange
+    }
+    */
