1d9436400bfbf0d9263a31f4c4ac9169dcfd411a
jain20 <jain.akanksha20@gmail.com>
2020-12-04 12:33:20 -0600

    Adding loading image and styling changes for drawer button/logo

--- safety-first-app/constants/Theme.js
+++ safety-first-app/constants/Theme.js
@@ -1,73 +1,74 @@
 export default {
-  COLORS: {
-    DEFAULT: '#172B4D',
-    PRIMARY: '#5E72E4',
-    SECONDARY: '#F7FAFC',
-    LABEL: '#FE2472',
-    INFO: '#11CDEF',
-    ERROR: '#F5365C',
-    SUCCESS: '#2DCE89',
-    WARNING: '#FB6340',
-    TEXT: '#32325D',
-    MUTED: '#8898AA',
-    INPUT: '#DCDCDC',
-    INPUT_SUCCESS: '#7BDEB2',
-    INPUT_ERROR: '#FCB3A4',
-    ACTIVE: '#5E72E4', //same as primary
-    BUTTON_COLOR: '#9C26B0', //wtf
-    PLACEHOLDER: '#9FA5AA',
-    SWITCH_ON: '#5E72E4',
-    SWITCH_OFF: '#D4D9DD',
-    GRADIENT_START: '#6B24AA',
-    GRADIENT_END: '#AC2688',
-    PRICE_COLOR: '#EAD5FB',
-    BORDER_COLOR: '#E7E7E7',
-    BLOCK: '#E7E7E7',
-    ICON: '#172B4D',
-    HEADER: '#525F7F',
-    BORDER: '#CAD1D7',
-    WHITE: '#FFFFFF',
-    BLACK: '#000000',
-    TWITTER: '#1DA1F2',
-    FACEBOOK: '#3B5999',
-    DRIBBBLE: '#EA4C89'
-  }
-};
+//   COLORS: {
+//     DEFAULT: '#172B4D',
+//     PRIMARY: '#013B82',
+//     SECONDARY: '#F7FAFC',
+//     LABEL: '#FE2472',
+//     INFO: '#11CDEF',
+//     ERROR: '#F5365C',
+//     SUCCESS: '#2DCE89',
+//     WARNING: '#FB6340',
+//     TEXT: '#32325D',
+//     MUTED: '#8898AA',
+//     INPUT: '#DCDCDC',
+//     INPUT_SUCCESS: '#7BDEB2',
+//     INPUT_ERROR: '#FCB3A4',
+//     ACTIVE: '#5E72E4', //same as primary
+//     BUTTON_COLOR: '#9C26B0', //wtf
+//     PLACEHOLDER: '#9FA5AA',
+//     SWITCH_ON: '#5E72E4',
+//     SWITCH_OFF: '#D4D9DD',
+//     GRADIENT_START: '#6B24AA',
+//     GRADIENT_END: '#AC2688',
+//     PRICE_COLOR: '#EAD5FB',
+//     BORDER_COLOR: '#E7E7E7',
+//     BLOCK: '#E7E7E7',
+//     ICON: '#172B4D',
+//     HEADER: '#525F7F',
+//     BORDER: '#CAD1D7',
+//     WHITE: '#FFFFFF',
+//     BLACK: '#000000',
+//     TWITTER: '#1DA1F2',
+//     FACEBOOK: '#3B5999',
+//     DRIBBBLE: '#EA4C89'
+//   }
+// };
 
 
 /*
 With Visa Blue
 */
-// COLORS: {
-//   DEFAULT: '#012A82',
-//   PRIMARY: '#012A82',
-//   SECONDARY: '#F7FAFC',
-//   LABEL: '#012A82',
-//   INFO: '#11CDEF',
-//   ERROR: '#F5365C',
-//   SUCCESS: '#2DCE89',
-//   WARNING: '#FB6340',
-//   TEXT: '#32325D',
-//   MUTED: '#8898AA',
-//   INPUT: '#DCDCDC',
-//   INPUT_SUCCESS: '#7BDEB2',
-//   INPUT_ERROR: '#FCB3A4',
-//   ACTIVE: '#012A82', //same as primary
-//   BUTTON_COLOR: '#192061', //wtf
-//   PLACEHOLDER: '#9FA5AA',
-//   SWITCH_ON: '#5E72E4',
-//   SWITCH_OFF: '#D4D9DD',
-//   GRADIENT_START: '#6B24AA',
-//   GRADIENT_END: '#AC2688',
-//   PRICE_COLOR: '#EAD5FB',
-//   BORDER_COLOR: '#E7E7E7',
-//   BLOCK: '#E7E7E7',
-//   ICON: '#172B4D',
-//   HEADER: '#525F7F',
-//   BORDER: '#CAD1D7',
-//   WHITE: '#FFFFFF',
-//   BLACK: '#000000',
-//   TWITTER: '#1DA1F2',
-//   FACEBOOK: '#3B5999',
-//   DRIBBBLE: '#EA4C89'
-// }
+COLORS: {
+  DEFAULT: '#013B82',
+  PRIMARY: '#013B82',
+  SECONDARY: '#F7FAFC',
+  LABEL: '#013B82',
+  INFO: '#11CDEF',
+  ERROR: '#F5365C',
+  SUCCESS: '#2DCE89',
+  WARNING: '#FB6340',
+  TEXT: '#32325D',
+  MUTED: '#8898AA',
+  INPUT: '#DCDCDC',
+  INPUT_SUCCESS: '#7BDEB2',
+  INPUT_ERROR: '#FCB3A4',
+  ACTIVE: '#013B82', //same as primary
+  BUTTON_COLOR: '#192061', //wtf
+  PLACEHOLDER: '#9FA5AA',
+  SWITCH_ON: '#5E72E4',
+  SWITCH_OFF: '#D4D9DD',
+  GRADIENT_START: '#6B24AA',
+  GRADIENT_END: '#AC2688',
+  PRICE_COLOR: '#EAD5FB',
+  BORDER_COLOR: '#E7E7E7',
+  BLOCK: '#E7E7E7',
+  ICON: '#172B4D',
+  HEADER: '#525F7F',
+  BORDER: '#CAD1D7',
+  WHITE: '#FFFFFF',
+  BLACK: '#000000',
+  TWITTER: '#1DA1F2',
+  FACEBOOK: '#3B5999',
+  DRIBBBLE: '#EA4C89'
+}
+};
